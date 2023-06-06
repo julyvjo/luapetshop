4c42eda7b4fe41b42c1da0f1642e2bbe7cfc832c
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-06-06 03:48:12 -0300

    Remove goToApp functionality
    This was no longer needed.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -33,7 +33,6 @@ if (nav)
     {
         element.addEventListener("click", (e) =>
         {
-            // goToApp(e.target.textContent);
             goToPage(e.target.textContent);
         });
     });
@@ -47,31 +46,6 @@ headerNavDivChildren.forEach(element =>
         goToPage(e.target.textContent);
     });
 });
-
-function goToApp(buttonId)
-{    
-    let appContainer = document.getElementById("app");
-    
-    // If an appContainer element does not exist; create a container
-    if (appContainer === null)
-    {
-        const container = document.createElement("div");
-        container.textContent = `Acá iría la acción o app para ${buttonId}`;
-        container.id = "app";
-
-        body.insertBefore(container, footer);
-        
-        // Here I would code the actual app invoke
-    }
-    else
-    {
-        // Potentially could add here a transition between apps
-
-        appContainer.textContent = `Acá iría la acción o app para ${buttonId}`;
-
-        // Here I would code the actual app invoke
-    }
-}
 
 function goToPage(buttonId)
 {
