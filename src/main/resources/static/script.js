6957e24f415385965e69b0dce6300400184bbdd9
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-06-06 04:21:12 -0300

    Add editor modal creation functionality

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,6 +2,7 @@ const body = document.body;
 const headerNav = document.getElementById('headerNav');
 const headerNavDivChildren = headerNav.querySelectorAll('div');
 const nav = document.getElementById('nav');
+const app = document.getElementById('app');
 const botonBuscador = document.getElementById('botonBuscador');
 const inputBuscador = document.getElementById('inputBuscador');
 const editorGeneral = document.getElementById('botonEditorGeneral');
@@ -23,7 +24,6 @@ headerNavDivChildren.forEach(element =>
 {
     element.addEventListener("click", (e) =>
     {
-        // goToApp(e.target.textContent);
         goToPage(e.target.textContent);
     });
 });
@@ -108,5 +108,68 @@ botonEditorGeneral.addEventListener("click", (e) =>
 
 function abrirEditor()
 {
+    // Create modal container
+    const modalContainer = document.createElement("div");
+    modalContainer.classList.add("modal", "fade");
+    modalContainer.setAttribute("tabindex", "-1");
+    modalContainer.setAttribute("role", "dialog");
+    modalContainer.setAttribute("aria-labelledby", "editorModalLabel");
+    modalContainer.setAttribute("aria-hidden", "true");
 
+    // Create modal dialog
+    const modalDialog = document.createElement("div");
+    modalDialog.classList.add("modal-dialog");
+    modalDialog.setAttribute("role", "document");
+
+    // Create modal content
+    const modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
+
+    // Create modal header
+    const modalHeader = document.createElement("div");
+    modalHeader.classList.add("modal-header");
+
+    // Create modal title
+    const modalTitle = document.createElement("h5");
+    modalTitle.classList.add("modal-title");
+    modalTitle.setAttribute("id", "editorModalLabel");
+    modalTitle.textContent = "Editor";
+
+    // Create modal close button
+    const modalCloseButton = document.createElement("button");
+    modalCloseButton.classList.add("close");
+    modalCloseButton.setAttribute("type", "button");
+    modalCloseButton.setAttribute("data-dismiss", "modal");
+    modalCloseButton.setAttribute("aria-label", "Close");
+    modalCloseButton.innerHTML = `<span aria-hidden="true">&times;</span>`;
+
+    modalHeader.appendChild(modalTitle);
+    modalHeader.appendChild(modalCloseButton);
+
+    // Create modal body
+    const modalBody = document.createElement("div");
+    modalBody.classList.add("modal-body");
+    modalBody.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+    // Append modal components
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+    modalDialog.appendChild(modalContent);
+    modalContainer.appendChild(modalDialog);
+
+    // Append modal container to #app
+    app.appendChild(modalContainer);
+
+    // Show the modal
+    modalContainer.classList.add("show");
+    modalContainer.style.display = "block";
+    modalContainer.removeAttribute("aria-hidden");
+
+    // Add click event listener to close the modal
+    modalCloseButton.addEventListener("click", () =>
+    {
+        modalContainer.classList.remove("show");
+        modalContainer.style.display = "none";
+        modalContainer.setAttribute("aria-hidden", "true");
+    });
 }
