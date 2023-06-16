c31c76e43aa482f48372344c8f130e76fa90278d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-06-16 04:02:56 -0300

    Add modalNuevoProveedor to proveedor.html

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -141,6 +141,22 @@ if (nuevoProducto)
 }
 // **************************************************************
 
+// NUEVO PROVEEDOR BUTTON
+
+const nuevoProveedor = document.getElementById('botonNuevoProveedor');
+
+if (nuevoProveedor)
+{
+    nuevoProveedor.addEventListener("click", (e) =>
+    {
+        mostrarModal("modalNuevoProveedor");
+    });
+
+    // I implemented it like this because script.js has "defer" attribute.
+    // If in the future this changes, this needs to be implemented inside an Event Listener.
+}
+// **************************************************************
+
 // CERRAR CAJA BUTTON
 
 const cerrarCaja = document.getElementById('botonCerrarCaja');
