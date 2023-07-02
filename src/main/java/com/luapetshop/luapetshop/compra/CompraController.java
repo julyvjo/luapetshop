cc1fb0dbb59ecc58b7d1f4d41d9bd0c55a4f09f2
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-07-02 04:50:13 -0300

    Agregar metodos de pago a COMPRA

--- src/main/java/com/luapetshop/luapetshop/compra/CompraController.java
+++ src/main/java/com/luapetshop/luapetshop/compra/CompraController.java
@@ -1,5 +1,6 @@
 package com.luapetshop.luapetshop.compra;
 
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,13 +8,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.luapetshop.luapetshop.model.MedioPago;
+import com.luapetshop.luapetshop.model.MedioPagoService;
+
 @Controller
 public class CompraController {
 	private CompraService compraService;
+    private MedioPagoService medioPagoService;
 	
 	@Autowired
-	public CompraController(CompraService compraService) {
+	public CompraController(CompraService compraService, MedioPagoService medioPagoService) {
 		this.compraService = compraService;
+        this.medioPagoService = medioPagoService;
 	}
 	
 	
@@ -25,9 +31,12 @@ public class CompraController {
 		
 		//model.addAttribute("proveedores", proveedores);
 		//model.addAttribute("medios_pago", pageProducto.getContent());
+
+        List<MedioPago> mediospago = medioPagoService.getMediosPago();
+		model.addAttribute("mediospago", mediospago);
+		
+		// System.out.println(mediospago.get(0).getNombre());
 		
 		return "compra";
 	}
-	
-	
 }
