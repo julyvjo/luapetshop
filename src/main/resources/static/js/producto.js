f8939691cc573d5ca09896ecf5a93c6a3c0799a9
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-07-09 14:43:49 -0300

    Add botonEditar functionality to pre-charge form

--- src/main/resources/static/js/producto.js
+++ src/main/resources/static/js/producto.js
@@ -4,16 +4,61 @@ const arrayBotonEditar = document.querySelectorAll('.botonEditar');
 
 if (arrayBotonEditar.length !== 0)
 {
-    arrayBotonEditar.forEach(element =>
+    arrayBotonEditar.forEach(fila =>
     {
-        element.addEventListener("click", (e) =>
+        fila.addEventListener("click", (e) =>
         {
             mostrarModal("modalEditorLinea");
+            cargarEditor(fila);
         });    
     });
 
     // I implemented it like this because script.js has "defer" attribute.
     // If in the future this changes, this needs to be implemented inside an Event Listener.
+}
+
+function cargarEditor(fila)
+{
+    /*
+        No es muy prolijo, pero con esto evito agregar atributos al html o llamar a la DB.
+    */
+
+    const tr = fila.parentNode.parentNode.parentNode;
+    const h5 = tr.querySelectorAll("h5");
+
+    // h5[0] = imagen; h5[h5.length - 1] = botón editar.
+    
+    // h5.forEach((element, index) =>
+    // {
+    //     if (index === 0 || index === h5.length - 1)
+    //         return;
+
+    //     console.log(element.textContent);
+    // });
+
+    const nombreProducto = document.getElementById("modalEditorLineaNombreProducto");
+    nombreProducto.value = h5[1].textContent;
+
+    const descripcionProducto = document.getElementById("modalEditorLineaDescripcionProducto");
+    descripcionProducto.value = h5[2].textContent;
+
+    const precioCompra = document.getElementById("modalEditorLineaPrecioCompra");
+    precioCompra.value = h5[3].textContent;
+
+    const porcentajeGanancia = document.getElementById("modalEditorLineaPorcentajeGanancia");
+    porcentajeGanancia.value = h5[4].textContent;
+
+    const gananciaProducto = document.getElementById("modalEditorLineaGananciaProducto");
+    gananciaProducto.value = h5[5].textContent;
+
+    //  Tipo de stock aún está pendiente saber cómo se va a implementar.
+    // const tipoStock = document.getElementById("modalEditorLineaNombreProducto");
+
+    const cantidadStock = document.getElementById("modalEditorLineaCantidadStock");
+    cantidadStock.value = h5[6].textContent;
+
+    //  Aún queda pendiente ver cómo implementar las imágenes de cada producto.
+    //  const imagenProducto = document.getElementById("modalEditorLineaNombreProducto");
 }
 // **************************************************************
 
