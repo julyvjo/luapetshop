4fa75035a0dd75df30e396dcb00c1b586f2c3e6b
julyvjo <julyvjo@gmail.com>
2023-07-13 23:26:22 -0300

    DB rework SQL, cambios a compras y producto

--- src/main/java/com/luapetshop/luapetshop/compra/CompraService.java
+++ src/main/java/com/luapetshop/luapetshop/compra/CompraService.java
@@ -1,22 +1,63 @@
 package com.luapetshop.luapetshop.compra;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.luapetshop.luapetshop.model.MedioPago;
+import com.luapetshop.luapetshop.proveedor.Proveedor;
 import com.luapetshop.luapetshop.repository.ICompraRepository;
+import com.luapetshop.luapetshop.repository.IMedioPagoRepository;
+import com.luapetshop.luapetshop.repository.IProveedorRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class CompraService {
 	private ICompraRepository compraRepository;
+	private IProveedorRepository proveedorRepository;
+	private IMedioPagoRepository medioPagoRepository;
 	
 	@Autowired
-	public CompraService(ICompraRepository compraRepository) {
+	public CompraService(ICompraRepository compraRepository, IProveedorRepository proveedorRepository,
+			IMedioPagoRepository medioPagoRepository) {
+		super();
 		this.compraRepository = compraRepository;
+		this.proveedorRepository = proveedorRepository;
+		this.medioPagoRepository = medioPagoRepository;
 	}
-	
+
 	public List<Compra> getCompras() {
 		return compraRepository.findAll();
+	}
+
+
+	@Transactional
+	public void createNewCompra(Map<String, Object> datos) {
+		//preparar compra
+		Compra compra = new Compra();
+		
+		//proveedor
+		int proveedor_id = (int) datos.get("proveedor_id");
+		Optional<Proveedor> proveedor = proveedorRepository.findById(proveedor_id);
+		proveedor.get();
+		
+		compra.setTotal((double)datos.get("total"));
+		
+		//medio_pago
+		int medio_pago_id = (int) datos.get("id_medio_pago");
+		Optional<MedioPago> optionalMedioPago = medioPagoRepository.findById(medio_pago_id);
+		MedioPago medioPago = optionalMedioPago.get();
+		
+		//impactar en cuenta
+		medioPago.getCuenta(); //<- impactar saldo
+		
 	}
 
 }
--- src/main/java/com/luapetshop/luapetshop/producto/ProductoService.java
+++ src/main/java/com/luapetshop/luapetshop/producto/ProductoService.java
@@ -1,6 +1,7 @@
 package com.luapetshop.luapetshop.producto;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.luapetshop.luapetshop.repository.IProductoRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ProductoService {
@@ -44,5 +47,37 @@ public class ProductoService {
 	public Optional<Producto> getProducto(int id) {
 		
 		return productoRepository.findById(id);
+	}
+
+	@Transactional
+	public void agregarProducto(Map<String, Object> datos) {
+				
+		try {			
+			Producto producto = map2Producto(datos);
+			productoRepository.save(producto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Producto map2Producto(Map<String, Object> datos) {
+		Producto producto = new Producto();
+		
+		int id_producto = (int) datos.get("id_producto");
+		//id_producto == 0 --> producto nuevo
+		if( id_producto != 0 ) {
+			producto.setId_producto(id_producto);
+		}
+
+		producto.setNombre( (String) datos.get("nombre"));
+		producto.setDescripcion((String) datos.get("descripcion"));
+		producto.setPrecio_compra((double) datos.get("precio_compra"));
+		producto.setRentabilidad((double) datos.get("rentabilidad"));
+		//producto.setCategoria(null);
+		//producto.setGanancia(null);
+		producto.setStock((int) datos.get("stock"));
+        //imagen (almacenar imagen, guardar ruta en producto.imagen
+		
+		return producto;
 	}
 }
