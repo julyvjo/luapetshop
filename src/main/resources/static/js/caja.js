aaae9191bf730205e2993a3829c72f9ba3ba4ab8
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-07-28 20:47:48 -0300

    Add detalles button functionality in CAJA
    This button now redirects towards respective page where all details are shown

--- src/main/resources/static/js/caja.js
+++ src/main/resources/static/js/caja.js
@@ -1,3 +1,22 @@
+// DETALLES BUTTON
+
+const DETALLES_BUTTONS = document.querySelectorAll(".appCajaDetalles");
+
+DETALLES_BUTTONS.forEach(element => 
+{
+    element.addEventListener("click", () =>
+    {
+        const ID_CUENTA = element.getAttribute("data-id-cuenta");
+        const LINK = new URL(`/caja/${ID_CUENTA}`, window.location.origin);
+        
+        window.location.href = LINK;
+    });
+});
+// **************************************************************
+
+
+
+
 // CERRAR CAJA BUTTON
 
 const cerrarCaja = document.getElementById('botonCerrarCaja');
