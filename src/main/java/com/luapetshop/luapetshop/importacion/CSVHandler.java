766adb0f8f81cabd363ecb06e63ae5e0560d8a20
julyvjo <julyvjo@gmail.com>
2023-08-14 22:42:49 -0300

    procesamiento de importacion de precios por csv

--- src/main/java/com/luapetshop/luapetshop/importacion/CSVHandler.java
+++ src/main/java/com/luapetshop/luapetshop/importacion/CSVHandler.java
@@ -1,24 +1,79 @@
 package com.luapetshop.luapetshop.importacion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+
 import org.springframework.web.multipart.MultipartFile;
 
-//import com.luapetshop.luapetshop.importacion.CSVProcessor.CSVProcessorPrecios;
-//import com.luapetshop.luapetshop.importacion.CSVProcessor.CSVProcessorTodo;
+import com.luapetshop.luapetshop.producto.Producto;
+import com.luapetshop.luapetshop.proveedor.Proveedor;
+import com.luapetshop.luapetshop.repository.IProductoRepository;
 
 public class CSVHandler {
-	
-	public String applyCSV(MultipartFile file, String tipo) throws Exception {
-		CSVProcessor proc = null;
-		
-//		if(tipo == "todo")
-//			//proc = new CSVProcessorTodo();
-//		else if(tipo == "precios")
-//			//proc = new CSVProcessorPrecios();
-//		else 
-//			throw new Exception("El tipo especificado es incorrecto"); 
-//		
-		return proc.process(file);
+	private MultipartFile file;
+	private IProductoRepository productoRepository;
+	Proveedor proveedor;
+
+	public CSVHandler(MultipartFile file, IProductoRepository productoRepository, Proveedor proveedor) {
+		super();
+		this.file = file;
+		this.productoRepository = productoRepository;
+		this.proveedor = proveedor;
 	}
+
+	public String process() {
+    	StringBuilder sb = new StringBuilder();
+    	int id_proveedor = this.proveedor.getId_proveedor();
+		
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+            	//los campos son: codigo y precio, el proveedor viene aparte
+                String[] fields = line.split(";");
+                String codigo = fields[0];
+                String precio = fields[1]; //nuevo precio de compra a proveedor
+                
+                //obtener producto
+                Optional<Producto> opt_producto = productoRepository.findProductoByCodigoAndProveedor(codigo, id_proveedor);
+                //validacion de existencia de producto
+                if(opt_producto.isEmpty()) {
+                	//el producto no existe, no se deberia crear en este caso porque es una actualizacion de precios
+                	sb.append("ERROR producto inexistente: " + codigo);
+                	continue;
+                }
+                Producto producto = opt_producto.get();
+                
+                //preparar valores
+                Double nuevo_precio_compra = Double.valueOf(precio);
+                Double rentabilidad = producto.getRentabilidad();
+                Double nuevo_precio_venta = nuevo_precio_compra * (rentabilidad + 1);
+                Double nueva_ganancia = nuevo_precio_venta - nuevo_precio_compra;
+                //setear valores
+                producto.setPrecio_compra(nuevo_precio_compra);
+                producto.setPrecio_venta(nuevo_precio_venta);
+                producto.setGanancia(nueva_ganancia);
+                
+                try {
+                	productoRepository.save(producto);
+                	sb.append("Actualizado producto [" + producto.getCodigo() + "] " + producto.getNombre() + ": precio_compra = " + producto.getPrecio_compra() + " ; precio_venta = " + producto.getPrecio_venta());
+				} catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+					System.out.println(e.getMessage());
+					sb.append("ERROR al actualizar producto: " + producto.getCodigo());
+				}
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+		return sb.toString();
+    }
+	
+	
 }
 
 		
--- src/main/java/com/luapetshop/luapetshop/repository/IProductoRepository.java
+++ src/main/java/com/luapetshop/luapetshop/repository/IProductoRepository.java
@@ -1,12 +1,15 @@
 package com.luapetshop.luapetshop.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.luapetshop.luapetshop.producto.Producto;
+import com.luapetshop.luapetshop.proveedor.Proveedor;
 
 public interface IProductoRepository extends JpaRepository<Producto, Integer> {
 
@@ -15,5 +18,11 @@ public interface IProductoRepository extends JpaRepository<Producto, Integer> {
 	Page<Producto> findByNombreContaining(String nombre, Pageable pageable);
 	
 	Page<Producto> findAll(Pageable pageable);
+
+	@Query(value = "SELECT * FROM producto WHERE"
+			+ " codigo = ?1 AND"
+			+ " id_proveedor = ?2"
+			+ " limit 1", nativeQuery = true)
+	Optional<Producto> findProductoByCodigoAndProveedor(String codigo, int id_proveedor);
 
 }
