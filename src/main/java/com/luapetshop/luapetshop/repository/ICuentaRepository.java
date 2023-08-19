e69aa48e368601bf479db1ab03d1bbaa042e237d
julyvjo <julyvjo@gmail.com>
2023-08-19 16:55:36 -0300

    fix query cuentas

--- src/main/java/com/luapetshop/luapetshop/repository/ICuentaRepository.java
+++ src/main/java/com/luapetshop/luapetshop/repository/ICuentaRepository.java
@@ -10,11 +10,10 @@ import com.luapetshop.luapetshop.cuenta.Cuenta;
 public interface ICuentaRepository extends JpaRepository<Cuenta, Integer> {
 
 	@Query(value = "SELECT SUM(ganancia) FROM venta"
-			+ " where fecha >= (SELECT fecha_apertura FROM cuenta ORDER BY fecha desc limit 1)", nativeQuery = true)
+			+ " where fecha >= (SELECT fecha_apertura FROM cuenta ORDER BY fecha_apertura desc limit 1)", nativeQuery = true)
 	Double findGananciaTotalDelDia();
 
-	@Query(value = "SELECT fecha_apertura "
-			+ "FROM cuenta ORDER BY fecha desc limit 1)", nativeQuery = true)
+	@Query(value = "SELECT max(fecha_apertura) FROM cuenta", nativeQuery = true)
 	LocalDateTime findFechaApertura();
 
 }
