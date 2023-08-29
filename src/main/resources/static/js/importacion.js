afb9bbb0294aecaa042980d47a2b14e0edb99f41
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-08-29 02:41:51 -0300

    Redesign IMPORTACION visual representation

--- src/main/resources/static/js/importacion.js
+++ src/main/resources/static/js/importacion.js
@@ -1,201 +1,62 @@
-// SEARCHBAR INPUT
+// Función para realizar el POST request al servidor
 
-const inputBuscador = document.getElementById('inputBuscador');
+function enviarArchivoPDF() {
+    // Crea un objeto FormData y agrega el archivo PDF que deseas enviar
 
-if (inputBuscador)
-{
-    inputBuscador.focus();
+    var formData = new FormData();
+    formData.append("pdfFile", document.getElementById("pdfFileInput").files[0]);
+    formData.append("pages", document.getElementById("pages").value);
 
-    inputBuscador.addEventListener("keydown", (e) =>
-    {
-        if (e.key === "Enter")
-        {
-          e.preventDefault();
-          chequearBuscador();
-        }
-        else // Maybe I should use some regEx to improve this...
-        {
-            // e.preventDefault(); // This will come useful when using ctrl/shift/alt modifications
-            // chequearBuscador(inputBuscador.value + e.key);
-            // console.log(inputBuscador.value + e.key);
-        }
+    // Realiza el POST request al servidor utilizando fetch
+
+    fetch("/upload_pdf", {
+        method: "POST",
+        body: formData
+    })
+
+    .then(response => response.blob())
+
+    .then(blob => {
+        // Genera un enlace para descargar el archivo CSV
+        var url = URL.createObjectURL(blob);
+        var a = document.createElement("a");
+        a.href = url;
+        a.download = "datos.csv";
+        a.click();
+        // Libera el objeto URL creado para evitar pérdidas de memoria
+        URL.revokeObjectURL(url);
+    })
+
+    .catch(error => {
+        // Manejo de errores en caso de que ocurra un problema con el fetch
+        console.error(error);
     });
-
-    // I implemented it like this because script.js has "defer" attribute.
-    // If in the future this changes, this needs to be implemented inside an Event Listener.
-}
-// **************************************************************
-
-
-
-// SEARCHBAR BUTTON
-
-const botonBuscador = document.getElementById('botonBuscador');
-
-if (botonBuscador)
-{
-    botonBuscador.addEventListener("click", (e) =>
-    {
-        chequearBuscador();
-    });
-
-    // I implemented it like this because script.js has "defer" attribute.
-    // If in the future this changes, this needs to be implemented inside an Event Listener.
-}
-// **************************************************************
-
-
-
-// SEARCH FUNCTIONS
-
-function chequearBuscador()
-{
-    if (inputBuscador.value !== "")
-        buscadorGoToPage(inputBuscador.value);
-    else
-        console.log("WARNING: Empty search bar!");
 }
 
-function buscadorGoToPage(contenido)
-{
-    //console.log("HELLO WORLD");
-    let actualurl = new URL(window.location.href);
-    // let nombre = actualurl.searchParams.get('nombre');
-    // // console.log('parametro nombre =' + nombre);
+function enviarArchivoCSV() {
+    const fileInput = document.getElementById('csvFileInput');
+    const tipoSelect = document.getElementById('proveedor');
 
-    actualurl.searchParams.set('nombre',contenido);
-    actualurl.searchParams.set('page',1);
-    // // console.log('nueva url =' + actualurl.toString());
-    window.location.href = actualurl.toString();        
-}
-// **************************************************************
+    const selectedFile = fileInput.files[0];
+    const selectedTipo = tipoSelect.value;
 
+    if (selectedFile) {
+        const formData = new FormData();
+        formData.append('csvFile', selectedFile);
+        formData.append('tipo', selectedTipo);
 
-
-// NUEVO PROVEEDOR BUTTON
-
-const nuevoProveedor = document.getElementById('botonNuevoProveedor');
-
-if (nuevoProveedor)
-{
-    nuevoProveedor.addEventListener("click", (e) =>
-    {
-        mostrarModal("modalNuevoProveedor");
-    });
-
-    // I implemented it like this because script.js has "defer" attribute.
-    // If in the future this changes, this needs to be implemented inside an Event Listener.
-}
-// **************************************************************
-
-
-
-// MODAL NUEVO PROVEEDOR FUNCTIONS
-
-const nuevoProveedorPrecioCompra = document.getElementById("modalNuevoProveedorPrecioCompra");
-
-nuevoProveedorPrecioCompra.addEventListener("change", () =>
-{
-    console.log("nuevoProveedorPrecioCompra cambió su valor");
-
-    if (validarPrecioCompra(nuevoProveedorPrecioCompra.value) === false)
-    {
-        console.log("ERROR: Se espera un número entero (integer) o flotante (float)!");
-        nuevoProveedorPrecioCompra.value = "0.00";
-        return;
-    }
-
-    nuevoProveedorPrecioCompra.value = nuevoProveedorPrecioCompra.value.replace(/,/g, ".");
-
-    nuevoProveedorPrecioCompra.value = parseFloat(nuevoProveedorPrecioCompra.value).toFixed(2);
-});
-
-function validarPrecioCompra(string)
-{
-    //  Regular expression para validar que string represente un número float
-    //  const numberRegex = /^[0-9]+$/;  //  Por si solo quisiera validar un número entero.
-    const numberRegex = /^[0-9]+([.,][0-9]+)?$/;;
-
-    return numberRegex.test(string);
-
-    // Ejemplos:
-    console.log(validateNumber("12345"));   // true
-    console.log(validateNumber("12.345"));  // true
-    console.log(validateNumber("12,345"));  // true
-    console.log(validateNumber("12,34.56"));// false (múltiples separadores)
-    console.log(validateNumber("abc"));     // false (contiene caracteres no válidos como letras)
-}
-
-const modalNuevoProveedorPrecioCompraButton = document.getElementById("modalNuevoProveedorPrecioCompraButton");
-
-modalNuevoProveedorPrecioCompraButton.addEventListener("click", () =>
-{
-    if (validarFormulario() === false)
-    {
-        window.alert("ERROR: Es necesario completar todos los campos!");
-        return;
-    }
-
-    console.log("formulario válido");
-    // cargarFormulario();
-});
-
-let infoNuevoProveedor = {};
-infoNuevoProveedor = iniciarInfoNuevoProveedor(iniciarInfoNuevoProveedor);
-
-/*
-    FORMATO DEFAULT infoNuevoProveedor
-
-    id_producto_proveedor: [INT],
-    nombre_proveedor: [STRING],
-    nombre_producto: [STRING],
-    precio_compra: [STRING], -> lo manipulo y valido como FLOAT
-*/
-
-function iniciarInfoNuevoProveedor(infoNuevoProveedor)
-{
-    infoNuevoProveedor = {
-        id_producto_proveedor: 0,
-        nombre_proveedor: "",
-        nombre_producto: "",
-        precio_compra: "0.00"
+        fetch('/upload_csv', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data); // Manejar la respuesta del servidor si es necesario
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    } else {
+        console.log('No se ha seleccionado un archivo CSV.');
     }
 }
-
-const nuevoProveedorIdProducto = document.getElementById("modalNuevoProveedorIdProducto");
-const nuevoProveedorNombreProveedor = document.getElementById("modalNuevoProveedorNombreProveedor");
-const nuevoProveedorNombreProducto = document.getElementById("modalNuevoProveedorNombreProducto");
-
-function validarFormulario()
-{
-    /*
-        En realidad no debería ser así esto, pero lo implemento de esta forma para que quede como plantilla para alguna versión futura.
-    */
-
-    if (nuevoProveedorIdProducto.value === "")
-    {
-        console.log("ERROR: ID del producto vacío!");
-        return false;
-    }
-
-    if (nuevoProveedorNombreProveedor.value === "")
-    {
-        console.log("ERROR: Nombre del proveedor vacío!");
-        return false;
-    }
-
-    if (nuevoProveedorNombreProducto.value === "")
-    {
-        console.log("ERROR: Nombre del producto vacío!");
-        return false;
-    }
-
-    if (nuevoProveedorPrecioCompra.value === "0.00")
-    {
-        console.log("ERROR: Precio de compra no asignado!");
-        return false;
-    }
-
-    return true;
-}
-// **************************************************************
